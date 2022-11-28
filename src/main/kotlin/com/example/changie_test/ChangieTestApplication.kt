3f4ec39b6fe70b9349d46c7d2d2356b45431de38
Ch4s3r <lamprecht.patrick1@gmail.com>
2022-11-28 17:20:10 +0100

    Init

---
+++ src/main/kotlin/com/example/changie_test/ChangieTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.changie_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChangieTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChangieTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/changie_test/ChangieTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.changie_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChangieTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
